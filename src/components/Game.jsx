0e7e7484e67d5376ebf4199d02acf2d3e336b149
wonkatin <j.drawers@gmail.com>
2021-11-11 17:15:53 -0600

    access API

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,32 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function(Game) {
+export default function Game() {
+    // const [randomNums, setRandomNums] = useState("")
+
+    useEffect(() => {
+        getRandomNums()
+        // fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+        //     .then((response) => {
+        //         console.log(response)
+        //         return response.text()
+        //     }).then((html) => {
+        //         console.log(html[0])
+        //     }).catch((error) => {
+        //         console.log(error)
+        //     })
+    }, [])
+
+    const getRandomNums = async() => {
+        try {
+            const response = await fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+            const text = await response.text()
+            console.log(text)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
     return (
-        <div> Hello Game</div>
+        <div> Mastermind</div>
     )
 }
