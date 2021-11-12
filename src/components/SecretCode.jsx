0a18936a31fbdb3ae710bf84f8857e6d5898263e
wonkatin <j.drawers@gmail.com>
2021-11-12 14:50:06 -0600

    update title, change game component to secret code component

---
+++ src/components/SecretCode.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SecretCode(props) {
+    console.log(props.randomNums)
+    // const secretCode = props.randomNums
+    return (
+        <div>{props.randomNums}</div>
+    )
+}
