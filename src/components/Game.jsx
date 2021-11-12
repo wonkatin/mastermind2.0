d62b558b5ac4af75f009a2ab75667c3dc6b1d728
wonkatin <j.drawers@gmail.com>
2021-11-11 18:04:37 -0600

    convert API response text to an array

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Game() {
-    // const [randomNums, setRandomNums] = useState("")
+    const [randomNums, setRandomNums] = useState("")
 
     useEffect(() => {
         getRandomNums()
@@ -20,13 +20,15 @@ export default function Game() {
         try {
             const response = await fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
             const text = await response.text()
-            console.log(text)
+            const nums = text.split('\n', text.length/2).map(el => parseInt(el))
+            console.log("secret code: " + nums)
+            setRandomNums(nums)
         }
         catch(error) {
             console.log(error)
         }
     }
     return (
-        <div> Mastermind</div>
+        <div>Mastermind</div>
     )
 }
