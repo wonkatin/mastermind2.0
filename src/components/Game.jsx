ece25bc48edc0259f3e842f8f79527929275d247
wonkatin <j.drawers@gmail.com>
2021-11-11 18:29:00 -0600

    update notes for API call

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -13,8 +13,8 @@ export default function Game() {
         try {
             const response = await fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
             const text = await response.text()
-            console.log(text)
             // API returns HTML text including a new line (\n) after each number
+            console.log(text)
             // for example when originally converted to array using const array = [...text] returned ['5', '\n', '1', '\n', '5', '\n', '1', '\n']
             // I used split to create an array and the limit to remove the extra line (I will probably use a variable for the limit later)
             const array = text.split('\n', text.length/2)
