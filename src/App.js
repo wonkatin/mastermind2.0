72aebae6cadc97f70d1934d08faa3d715e1dd2e8
wonkatin <j.drawers@gmail.com>
2021-11-13 22:08:16 -0600

    update level variable

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
   // Access API for random number generator
   const getRandomNums = async() => {
       try {
-          const response = await fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+          const response = await fetch(`https://www.random.org/integers/?num=${level}&min=0&max=7&col=1&base=10&format=plain&rnd=new`)
           const text = await response.text()
           // API returns HTML text including a new line (\n) after each number
           // console.log(text)
