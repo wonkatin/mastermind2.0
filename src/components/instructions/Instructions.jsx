9bc4fb754e8f845c4612397ae24b4f906c9ad236
wonkatin <j.drawers@gmail.com>
2021-11-15 19:33:33 -0600

    instructions image added

--- src/components/instructions/Instructions.jsx
+++ src/components/instructions/Instructions.jsx
@@ -1,9 +1,15 @@
 import PlayButton from "./PlayButton";
+import image from "../../images/mastermindinstructions.png"
 
 export default function Instructions(props) {
     return (
         <div className="instructions">
-            Instructions
+            <div className="instructions-title">
+                Instructions
+            </div>
+            <div className="instructions-image">
+                <img className="image" src={image} alt="image of game play with instructions"/>
+            </div>
             <PlayButton setModal={props.setModal}/>
         </div>
     )
