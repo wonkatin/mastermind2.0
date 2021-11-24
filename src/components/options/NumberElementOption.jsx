83f9710b5af5463825b33901bfc3ba09c84cfd00
wonkatin <j.drawers@gmail.com>
2021-11-24 16:34:51 -0600

    move handleClick for option buttons

--- src/components/options/NumberElementOption.jsx
+++ src/components/options/NumberElementOption.jsx
@@ -1,4 +1,14 @@
 export default function NumberElementOption(props) {
+        // function for handleClick
+        // set virtual game board to the value and color at that row and number
+        const handleClick = () => {
+            props.gameBoard[props.rowCount][props.guessCount] = {value: props.value, color: props.color}
+            updateGuessCount()
+        }
+        // track guess count and use to know where we are in the row
+        const updateGuessCount = () => {
+             props.setGuessCount(props.guessCount + 1)
+        }
     // add onclick modal telling user to submit their guess
     // disable onClick if guess count hits level number 
     if (props.guessCount >= props.level) {
@@ -11,7 +21,7 @@ export default function NumberElementOption(props) {
         )
     } else {
         return (
-            <button className={`number-element hover ${props.color}`} onClick={props.handleClick}>
+            <button className={`number-element hover ${props.color}`} onClick={handleClick}>
                 <div className="number-text-container">
                     {props.value}
                 </div>
